c3cf2da9962b3bc0063761809a120f23c5be5ee7
Sugam Adhikari <sugam.19217113@gamil.com>
2024-01-08 23:23:47 +0545

    image row added

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,21 +1,64 @@
 import React from 'react';
 import backgroundImage from '../assets/Images/hero 1.png';
-import YellowButton from '../components/button'; 
+import YellowButton from '../components/button';
+import image_1 from '../assets/Images/Rectangle 1.png'
+import image_2 from '../assets/Images/Rectangle 1 (1).png'
+import image_3 from '../assets/Images/Rectangle 1 (2).png'
+import image_4 from '../assets/Images/Rectangle 1 (3).png'
 
 const Home: React.FC = () => {
   return (
-    <div className="relative flex items-center">
-      <img src={backgroundImage} alt="flower" />
-      <div className="absolute w-full">
-        <div className="container mx-4 md:mx-20 lg:mx-40 flex h-full">
-          <div className="max-w-full lg:max-w-80">
-            <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold text-gray-800 pb-2 md:pb-4">
-              Scandinavian Supremacy
-            </h1>
-            <p className="font-semibold mt-3 mb-8 text-xs md:text-sm">
+    <div>
+      {/* Hero Section */}
+      <div className="relative flex items-center">
+        <img src={backgroundImage} alt="flower" className="w-full h-auto rounded-lg" />
+        <div className="absolute">
+          <div className="container mx-4 md:mx-20 lg:mx-40 flex h-full">
+            <div className="max-w-full lg:max-w-80">
+              <h1 className="text-2xl md:text-3xl lg:text-5xl font-bold text-gray-800 pb-2 md:pb-4">
+                Scandinavian Supremacy
+              </h1>
+              <p className="font-semibold mt-3 mb-8 text-xs md:text-sm">
                 At Miralou we have turned traditional skin care ingredients lists on its head
-            </p>
-            <YellowButton />
+              </p>
+              <YellowButton />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Container with Four Images */}
+      <div className="container mx-auto mt-10 flex items-center justify-between p-20">
+        <div style={{ flex: '1' }} className="p-3">
+        <div className="mx-auto relative">
+            <img src={image_1} alt="Women" className="w-full h-auto squared-lg" />
+            <div className='absolute -bottom-8 left-0 right-0 max-w-40 bg-gray-200 mx-auto px-4 py-4 text-center'>
+              For Women
+            </div>
+          </div>
+        </div>
+        <div style={{ flex: '1' }} className="p-3">
+        <div className="mx-auto relative">
+            <img src={image_2} alt="Men" className="w-full h-auto squared-lg" />
+            <div className='absolute -bottom-8 left-0 right-0 max-w-40 bg-gray-200 mx-auto px-4 py-4 text-center'>
+              For Men
+            </div>
+          </div>
+        </div>
+        <div style={{ flex: '1' }} className="p-3">
+        <div className="mx-auto relative">
+            <img src={image_3} alt="Pregnant" className="w-full h-auto squared-lg" />
+            <div className='absolute -bottom-8 left-0 right-0 max-w-40 bg-gray-200 mx-auto px-4 py-4 text-center'>
+              For Pregnant
+            </div>
+          </div>
+        </div>
+        <div style={{ flex: '1' }} className="p-3">
+        <div className="mx-auto relative">
+            <img src={image_4} alt="Children" className="w-full h-auto squared-lg" />
+            <div className='absolute -bottom-8 left-0 right-0 max-w-40 bg-gray-200 mx-auto px-4 py-4 text-center'>
+              For Children
+            </div>
           </div>
         </div>
       </div>
