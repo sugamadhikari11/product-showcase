3b93580944058c6731a31fa8872ce0e46a9f8421
Sugam Adhikari <sugam.19217113@gamil.com>
2024-01-23 07:58:56 +0545

    latest added

--- src/api/products.ts
+++ src/api/products.ts
@@ -1,21 +1,22 @@
 import axios from "./axios"
 import { useEffect, useState } from "react"
 
-export const useProducts = () => {
+export const useProducts = (params: any) => {
+    const category = params.category;
     const [data, setData] = useState()
     const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         setLoading(true)
         axios
-            .get("/products?category=1&page")
+            .get("/products?category=" + category)
             .then(res => res.data)
             .then(data => {
                 setData(data)
             }).finally(() => {
                 setLoading(false)
             })
-    }, [])
+    }, [params.category])
     
     return {
         data: data,
